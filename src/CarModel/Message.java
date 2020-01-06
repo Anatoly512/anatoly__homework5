b92c5f2209a3d52a147985a722b9d3da059c2611
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2020-01-06 20:34:29 +0200

    New ARRAY of doors added

--- src/CarModel/Message.java
+++ src/CarModel/Message.java
@@ -3,7 +3,7 @@ package CarModel;
 public class Message {
         public static final String INPUT_DOORS = "Please, enter status DOOR  № ";
         public static final String INPUT_WINDOWS = "Please, enter status WINDOW  № ";
-        public static final String INPUT_OPEN_CLOSED = "\t Open - 1,  Closed - 2  :  ";
+        public static final String INPUT_OPEN_CLOSED = "  Open - 1,  Closed - 2  :  ";
         public static final String INPUT_ONE_OR_TWO = "\nPlease, enter only  1 (OPEN)  or  2 (CLOSED)  :  ";
         public static final String ERROR_INT_INPUT_MESSAGE = "\nВы ввели что-то не то!  Попробуйте ввести целое число. \n";
         public static final String WRONG_DURING_INPUT = " Something wrong during input a number!";
